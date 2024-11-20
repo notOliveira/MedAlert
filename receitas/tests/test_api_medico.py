94ed1b60dc590c79a78f8b144ed62cec044af549
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2024-11-20 20:42:35 -0300

    Otimizando o código

--- receitas/tests/test_api_medico.py
+++ receitas/tests/test_api_medico.py
@@ -2,51 +2,48 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework.test import APIClient
 from usuarios.models import Usuario
-from alarmes.models import Alarme
 from receitas.models import Receita
 from colorama import Fore, init
 
 init(autoreset=True)
 
+
 class ReceitaMedicoAPITestCase(TestCase):
 
     def setUp(self):
-        """Configuração inicial antes de cada teste."""
+        """Configuração inicial para cada teste."""
         self.client = APIClient()
 
         # Criar médico
-        self.medico = Usuario.objects.create(
+        self.medico = Usuario.objects.create_user(
             email="medico@example.com",
             username="medico01",
             user_type="MED",
             crm="123456",
             especialidade=1,
             estado="SP",
+            password="password",
         )
-        self.medico.set_password("password")
-        self.medico.save()
 
         # Criar paciente
-        self.paciente = Usuario.objects.create(
+        self.paciente = Usuario.objects.create_user(
             email="paciente@example.com",
             username="paciente01",
             user_type="PAC",
             idade=30,
+            password="password",
         )
-        self.paciente.set_password("password")
-        self.paciente.save()
 
-        # Autenticação do médico
+        # Autenticar o médico
         response = self.client.post(
-            reverse('login'),
+            reverse("login"),
             {"email": self.medico.email, "password": "password"},
-            format='json',
+            format="json",
         )
-        self.token_medico = response.data['access']
-        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token_medico}')
+        self.token_medico = response.data["access"]
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.token_medico}")
 
         test_name = self._testMethodName
-
         print(f"\n\n{Fore.YELLOW}==================== INÍCIO DO TESTE {test_name} ====================\n")
 
     def tearDown(self):
@@ -54,10 +51,9 @@ class ReceitaMedicoAPITestCase(TestCase):
         test_name = self._testMethodName
         print(f"\n\n{Fore.GREEN}==================== FINAL DO TESTE {test_name} ====================\n\n")
 
-    # Teste no endpoint /receitas/receita-alarme/
-    def test_criar_receita_e_alarme(self):
-        """Testa a criação de uma receita e um alarme como médico."""
-        url = reverse('api-receitas-receita-alarme')
+    def criar_receita_e_alarme(self):
+        """Função utilitária para criar uma receita com alarme."""
+        url = reverse("api-receitas-receita-alarme")
         data = {
             "paciente": self.paciente.email,
             "medicamento": "Paracetamol",
@@ -70,105 +66,64 @@ class ReceitaMedicoAPITestCase(TestCase):
                 "medicamento": "Paracetamol",
             },
         }
+        response = self.client.post(url, data, format="json")
 
-        response = self.client.post(url, data, format='json')
+        print(f"\nResponse: {response.data}")
+        return response
 
-        print(f'\nResponse: {response.data}')
-
+    def test_criar_receita_e_alarme(self):
+        """Testa a criação de uma receita com alarme por um médico."""
+        response = self.criar_receita_e_alarme()
         self.assertEqual(response.status_code, 201)
-        self.assertIn('receita', response.data)
-        self.assertIn('alarme', response.data)
+        self.assertIn("receita", response.data)
+        self.assertIn("alarme", response.data)
 
-        receita = Receita.objects.get(id=response.data['receita']['id'])
+        receita = Receita.objects.get(id=response.data["receita"]["id"])
 
-        print(f'\nReceita: {receita}')
-        
+        print(f"\nReceita: {receita}")
         self.assertEqual(receita.medicamento, "Paracetamol")
         self.assertEqual(receita.paciente, self.paciente)
         self.assertEqual(receita.medico, self.medico)
 
-        alarme = receita.alarme
-        self.assertIsNotNone(alarme)
-        self.assertEqual(alarme.medicamento, "Paracetamol")
-
-    # Testes no endpoint /receitas/
     def test_visualizar_receitas_como_medico(self):
-        """Testa se o médico consegue visualizar as receitas que prescreveu."""
-        # Criar uma receita para o paciente
-        
-        url = reverse('api-receitas-receita-alarme')
-        data = {
-            "paciente": self.paciente.email,
-            "medicamento": "Paracetamol",
-            "dose": "500mg",
-            "recomendacao": "Tomar após as refeições",
-            "alarme": {
-                "inicio": "2024-11-21T08:00:00Z",
-                "intervalo_horas": 8,
-                "duracao_dias": 5,
-                "medicamento": "Paracetamol",
-            },
-        }
-
-        response = self.client.post(url, data, format='json')
-
-        print(f'\nResponse: {response.data}')
-
+        """Testa se o médico consegue visualizar receitas que prescreveu."""
+        # Criar receita
+        response = self.criar_receita_e_alarme()
         self.assertEqual(response.status_code, 201)
 
-        # Listar receitas
-        url = reverse('api-receitas-preescritas')
-        response = self.client.get(url, format='json')
+        # Listar receitas prescritas
+        url = reverse("api-receitas-preescritas")
+        response = self.client.get(url, format="json")
 
         print(f"\nReceitas prescritas: {response.data}")
-        
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['medicamento'], "Paracetamol")
-        self.assertEqual(response.data[0]['paciente']['email'], self.paciente.email)
-        self.assertEqual(response.data[0]['medico']['email'], self.medico.email)
-        self.assertEqual(response.data[0]['recomendacao'], "Tomar após as refeições")
+
+        receita = response.data[0]
+        self.assertEqual(receita["medicamento"], "Paracetamol")
+        self.assertEqual(receita["paciente"]["email"], self.paciente.email)
+        self.assertEqual(receita["medico"]["email"], self.medico.email)
 
     def test_editar_receita_como_medico(self):
-        """Testa se o médico consegue editar uma receita."""
-        # Criar uma receita
-        url = reverse('api-receitas-receita-alarme')
-        data = {
-            "paciente": self.paciente.email,
-            "medicamento": "Paracetamol",
-            "dose": "500mg",
-            "recomendacao": "Tomar após as refeições",
-            "alarme": {
-                "inicio": "2024-11-21T08:00:00Z",
-                "intervalo_horas": 8,
-                "duracao_dias": 5,
-                "medicamento": "Paracetamol",
-            },
-        }
-
-        response = self.client.post(url, data, format='json')
-
+        """Testa se o médico consegue editar uma receita que prescreveu."""
+        # Criar receita
+        response = self.criar_receita_e_alarme()
         self.assertEqual(response.status_code, 201)
-        self.assertIn('receita', response.data)
-        self.assertIn('alarme', response.data)
+        receita_id = response.data["receita"]["id"]
 
-        receita = Receita.objects.get(id=response.data['receita']['id'])
-
-        print(f'\nReceita: {receita}')
+        receita = Receita.objects.get(id=receita_id)
+        print(f"\nReceita: {receita}")
 
         # Atualizar a receita
-        url = reverse('api-receitas-detail', args=[receita.id])
+        url = reverse("api-receitas-detail", args=[receita_id])
         data = {
             "recomendacao": "Tomar com leite para evitar dor de estômago",
             "dose": "300mg",
         }
-        response = self.client.patch(url, data, format='json')
+        response = self.client.patch(url, data, format="json")
         self.assertEqual(response.status_code, 200)
 
-        nova_receta = Receita.objects.get(id=receita.id)
-
-        print(f'\nNova receita: {nova_receta}')
-
         receita.refresh_from_db()
+        print(f"\nNova receita: {receita}")
         self.assertEqual(receita.recomendacao, "Tomar com leite para evitar dor de estômago")
         self.assertEqual(receita.dose, "300mg")
