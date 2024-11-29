c958442eb2216b13d9ca0e69910fc755e9862a36
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2024-11-29 20:18:19 -0300

    Mudando caractere

--- usuarios/views.py
+++ usuarios/views.py
@@ -116,7 +116,7 @@ class SetNewPasswordAPIView(generics.GenericAPIView):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         return Response(
-            {'success': True, 'message': 'Senha redefinida com sucesso.'},
+            {'success': True, 'message': 'Senha redefinida com sucesso!'},
             status=status.HTTP_200_OK
         )
 
